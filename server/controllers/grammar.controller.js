98f355e0f30369119fd89718d611e0f0f14854af
Griffin Lacek <griffinlacek@gmail.com>
2017-01-02 15:04:44 -0600

    Grammar error catch

--- server/controllers/grammar.controller.js
+++ server/controllers/grammar.controller.js
@@ -7,10 +7,11 @@ var lt = require('node-languagetool');
 * @returns void
 */
 exports.check = function(req, res) {
-  lt.check(
-    req.body.text,
-    req.body.fromLang
-  ).then(grammar => res.json(grammar));
+  lt.check(req.body.text, req.body.fromLang)
+  .then(grammar => res.json(grammar))
+  .catch(error => {
+    res.status(500).send(error);
+  });
 }
 
 /**
@@ -20,5 +21,9 @@ exports.check = function(req, res) {
 * @returns void
 */
 exports.languages = function(req, res) {
-  lt.languages().then(langs => res.json(langs));
+  lt.languages()
+  .then(langs => res.json(langs))
+  .catch(error => {
+    res.status(500).send(error);
+  });
 }

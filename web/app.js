5ffac197d718e21512c3ade8925bc8f9ebe964c3
Griffin Lacek <griffinlacek@gmail.com>
2017-01-05 16:40:04 -0600

    Browser history & app.js setup

---
+++ web/app.js
@@ -0,0 +1,8 @@
+// web/app.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router from './router';
+
+window.onload = () => {
+  ReactDOM.render(Router, document.getElementById('main'));
+};
--- web/router.js
+++ web/router.js
@@ -1,16 +1,18 @@
 // web/routes.js
 import React from 'react'
-import { Route, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
 import NotFoundPage from './components/NotFoundPage';
 
 
 const routes = (
-  <Route path="/" component={Layout}>
-    <IndexRoute component={IndexPage}/>
-    <Route path="*" component={NotFoundPage}/>
-  </Route>
+  <Router history={browserHistory} onUpdate={() => window.scrollTo(0, 0)}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={IndexPage}/>
+      <Route path="*" component={NotFoundPage}/>
+    </Route>
+  </Router>
 );
 
 export default routes;

01633045e0d70879b0c9223b3c0f92f4b87ec1d1
Griffin Lacek <griffinlacek@gmail.com>
2017-01-06 22:40:59 -0600

    Basic Language Controls Implemented

--- web/components/LangControls.js
+++ web/components/LangControls.js
@@ -4,27 +4,43 @@ import React from 'react';
 export default class LangControls extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {fromValue: 'en', toValue: 'es'};
+    this.state = {fromLang: 'en', toLang: 'es'};
 
     this.handleFromChange = this.handleFromChange.bind(this);
     this.handleToChange = this.handleToChange.bind(this);
-  }
-
-  handleFromChange(event) {
+    this.handleSwitch= this.handleSwitch.bind(this);
 
   }
 
-  handleToChange(event) {
+  handleFromChange(e) {
+    this.setState({fromLang: e.target.value});
+  }
 
+  handleToChange(e) {
+    this.setState({toLang: e.target.value});
+  }
+
+  handleSwitch(e) {
+    e.preventDefault();
+
+    this.setState({ fromLang: this.state.toLang, toLang: this.state.fromLang });
   }
 
   render() {
     return (
       <div className="controls-container">
-        <select className="fromLang" value={this.state.fromValue} onChange={this.handleFromChange}>
+        <select className="fromLang" value={this.state.fromLang} onChange={this.handleFromChange}>
+          <option value="en">English</option>
+          <option value="es">Spanish</option>
         </select>
 
-        <select className="toLang" value={this.state.toValue} onChange={this.handleToChange}>
+        <button onClick={this.handleSwitch}>
+          Switch
+        </button>
+
+        <select className="toLang" value={this.state.toLang} onChange={this.handleToChange}>
+          <option value="en">English</option>
+          <option value="es">Spanish</option>
         </select>
       </div>
     );

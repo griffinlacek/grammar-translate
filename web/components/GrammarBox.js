72834c12df761328a17ca668403a6601bc0a8fd3
Griffin Lacek <griffinlacek@gmail.com>
2017-01-08 17:19:15 -0600

    Parent/child logic rework (IndexPage & GrammarBox)

--- web/components/GrammarBox.js
+++ web/components/GrammarBox.js
@@ -1,51 +1,16 @@
 // web/components/GrammarBox.js
 import React from 'react';
-import 'whatwg-fetch';
-import { helpers } from '../helpers/Helpers';
 
 export default class GrammarBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { textValue: 'Type or paste your own text here to get grammar suggestions & translation to the language of your choice.' };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
-  handleChange(e) {
-    this.setState({ textValue: e.target.value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-
-    console.log(this.state.textValue);
-
-    fetch('/api/grammar', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        text: this.state.textValue,
-        fromLang: 'es'
-      })
-    })
-    .then(helpers.checkResponseStatus)
-    .then(helpers.parseJSON)
-    .then(json => {
-      console.log(json);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }
-
+  
   render() {
     return (
       <div className="grammar-container">
-        <form onSubmit={this.handleSubmit}>
-          <textarea value={this.state.textValue} onChange={this.handleChange} />
+        <form onSubmit={this.props.onSubmit}>
+          <textarea value={this.props.text} onChange={this.props.onChange} />
           <input type="submit" value="Check Text" />
         </form>
       </div>
--- web/components/IndexPage.js
+++ web/components/IndexPage.js
@@ -3,19 +3,53 @@ import React from 'react';
 import LangControls from './LangControls';
 import GrammarBox from './GrammarBox';
 import TranslateBox from './TranslateBox';
-
+import 'whatwg-fetch';
+import { helpers } from '../helpers/Helpers';
 
 export default class IndexPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = {
+      text: 'Type or paste your own text here to get grammar suggestions & translation to the language of your choice.'
+    };
+  }
+
+  updateText(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  submitText(e) {
+    e.preventDefault();
+
+    fetch('/api/grammar', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        text: this.state.text,
+        fromLang: 'es'
+      })
+    })
+    .then(helpers.checkResponseStatus)
+    .then(helpers.parseJSON)
+    .then(json => {
+      console.log(json);
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 
   render() {
     return (
       <div className="home">
         <LangControls />
-        <GrammarBox />
+        <GrammarBox
+          text={this.state.text}
+          onChange={this.updateText.bind(this)}
+          onSubmit={this.submitText.bind(this)}
+          />
         <TranslateBox />
       </div>
     );

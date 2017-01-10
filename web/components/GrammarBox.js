e8a0d461f8a43924b914f8e31484f276bdd74b20
Griffin Lacek <griffinlacek@gmail.com>
2017-01-10 15:39:00 -0600

    Textarea autosize module implemented

--- web/components/GrammarBox.js
+++ web/components/GrammarBox.js
@@ -1,5 +1,6 @@
 // web/components/GrammarBox.js
 import React from 'react';
+import Textarea from 'react-textarea-autosize';
 
 export default class GrammarBox extends React.Component {
   constructor(props) {
@@ -10,7 +11,7 @@ export default class GrammarBox extends React.Component {
     return (
       <div className="grammar-container">
         <form onSubmit={this.props.onSubmit}>
-          <textarea autoFocus value={this.props.text} onChange={this.props.onChange} />
+          <Textarea autoFocus value={this.props.text} onChange={this.props.onChange}></Textarea>
           <input type="submit" value="Check Text" />
         </form>
       </div>
